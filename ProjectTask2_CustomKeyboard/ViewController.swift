1f32aa24459dca7e95aced932de9b5b09a52e440
Atanu Panja <74297632+AtanuPanja@users.noreply.github.com>
2024-02-23 18:53:00 +0530

    Initial commit
    Added code files for the project
    
    Started working on basic layout

---
+++ ProjectTask2_CustomKeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectTask2_CustomKeyboard
+//
+//  Created by promact on 23/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
